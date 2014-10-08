aba3e89d1e38156d5308b59ee4d8b5361b459c01
Kris Reeves <kris.re@bbhmedia.com>
2014-10-08 14:05:00 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# What is it?
+
+A shim to make Mocha's reporter api suck less. It allows you to bind multiple reporters and specify streams for them to output to.
+
+# How does it work?
+
+It proxies the reporter events and temporarily replaces stdout (but not stderr) on the global process object before calling each reporter's event handler, putting things back when the handler returns.
+
+# How do I use it?
+
+    var Multiplexer = require('mocha-reporter-multiplexer')({
+        'require-string': stream
+    });
+    var mocha = new Mocha({
+        reporter: Multiplexer
+    });
+    // .. etc
+    mocha.run();
+
+# Why would I use it?
+
+Say you want to output an XML report of your test results. If you just run mocha and pipe it to a file, any stray console.log lines or error reports (that shouldn't have used console.log but do) will make your XML invalid. By cleanly segregating outputs, this shim allows you to safely output a separate XML file for use in Jenkins or whatever. It also lets you print one report to the screen and a different report type to a file, or even create multiple report files in different formats.
+
+# Potential gotchas
+
+This module incorrigably messes with globals and "private" properties of classes, and relies on Node implementation specifics. There are no guarantees this will ever work, now or in the future.
+
+In reality, as long as EventEmitter.prototype.emit continues to emit synchronously, and Node's Console class doesn't change the name of its captured stdout reference, things should be fine.
---
+++ index.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var Base = require('mocha/lib/reporters/base'),
+    EventEmitter = require('events').EventEmitter,
+    format = require('util').format;
+
+var runnerEvents = [ 'start', 'pass', 'pending', 'fail', 'end', 'hook', 'hook end', 'suite', 'suite end', 'test', 'test end' ];
+var _process = process;
+
+var PassThrough = require('stream').PassThrough;
+
+module.exports = function (reporters) {
+    function Multiplexer(runner) {
+        Base.call(this, runner);
+        
+        this._reporters = Object.keys(reporters).map(function (key) {
+            var Reporter = require(key),
+                ee = new EventEmitter;
+            
+            var stream = reporters[key],
+                reporter = new Reporter(ee);
+            
+            runnerEvents.forEach(function (event) {
+                runner.on(event, function () {
+                    var _stdout = console._stdout;
+                    console._stdout = stream;
+                    
+                    var proxy = { stdout: stream };
+                    proxy.__proto__ = _process;
+                    global.process = proxy;
+                    
+                    var i = arguments.length, args = new Array(i+1);
+                    while (i--) { args[i+1] = arguments[i]; }
+                    args[0] = event;
+                    
+                    ee.emit.apply(ee, args);
+                    
+                    global.process = _process;
+                    console._stdout = _stdout;
+                });
+            });
+        });
+    }
+    
+    return Multiplexer;
+};
